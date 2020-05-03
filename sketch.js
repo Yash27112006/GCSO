a973d98c9d2cf59f3a13533b9790e48654a14435
Yash27112006 <64268919+Yash27112006@users.noreply.github.com>
2020-05-03 20:25:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,47 @@
+var car,wall;
+var speed,weight;
+function setup() {
+  createCanvas(1600,400);
+  speed=random(55,90);
+  weight=random(400,1500);
+
+  wall=createSprite(1500,200,60,height/2);
+  wall.shapeColor=color("black");
+  
+  car=createSprite(50,200,50,50,weight);
+  car.velocityX=speed;
+  car.shapeColor=color("black");
+}
+
+function draw() {
+  background("pink");
+ // if(wall.x-car.x < (car.width+wall.width)/2) 
+  if(wall.x-car.x < wall.width/2+car.width/2 && car.x-wall.x<car.width/2+wall.width/2 ){
+     car.velocityX=0;
+     var deformation = 0.5 * weight * speed * speed/22509;
+     
+     textSize(40);
+     fill("black");
+
+     if(deformation>180){
+        car.shapeColor=color("red");
+        text("Rating: D ",650,200)
+        text("LETHAL FOR PASSENGERS",500,260)
+     }
+
+     if(deformation<180 && deformation>100){
+        car.shapeColor=color(230,230,0);
+        text("Rating: B ",655,200)
+        text("IT'A AVERAGE FOR PASSENGERS",410,260)
+     }
+
+     if(deformation<100){
+      car.shapeColor=color(0,255,0);
+      text("Rating: A ",660,200)
+      text("GOOD FOR PASSENGERS",500,260)
+   }
+  }
+  
+
+  drawSprites();
+}
